2d1fc801e3ba9b61e9ddbcad35e4a9ee1d22f87c
Your Name <blouismarceldidier@yahoo.fr>
2019-01-13 16:06:06 -0600

    I did it in a better way

--- Problem2.js
+++ Problem2.js
@@ -1,10 +1,12 @@
 //Create a function that’s passed a firstName and lastName.
 // Return lastName comma firstName.
 
-function name(a, b){
-    var x = a + b;
+function name(name1, name2){
+    var x = name1 +", "+ name2;
     return x;
 }
-var a = "firstName ";
-var b =  "lastname";
-console.log(name(a, b));
+var b = prompt("Firstname: ");
+var y =  prompt("Lastname");
+console.log(name(b,y));
+ // this program asks the user to write a firstname and a lastname
+//Then return both separate by comma;
